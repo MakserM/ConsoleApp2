417f157018a1526721820bf77e6fc6c4df59a78e
10_4BT_1 <10_4BT_1@DESKTOP-A8U3OEL>
2023-02-24 12:58:28 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculator.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w aplikacji KALKULATOR!");
+
+            Console.WriteLine("Podaj proszę 1 liczbę:");
+
+            var number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Jaką operację chcesz wykonać? Możliwe operacje to: '+', '-', '*', '/'.");
+
+            var operation = Console.ReadLine();
+
+            Console.WriteLine("Podaj proszę 2 liczbę:");
+
+            var number2 = int.Parse(Console.ReadLine());
+
+            var result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+                default:
+                    throw new Exception("Wybrałeś złą operację!");
+            }
+
+            Console.WriteLine($"Wynik Twojego działania to: {result}.");
+        }
+    }
+}
